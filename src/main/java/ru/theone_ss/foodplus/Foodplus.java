b1282a918217d739fbb1d39abb1d33af503aa3dc
Theoness1 <sasha.bolotskih2002@gmail.com>
2023-03-01 16:02:11 +0700

    add Food+ ItemGroup

--- src/main/java/ru/theone_ss/foodplus/Foodplus.java
+++ src/main/java/ru/theone_ss/foodplus/Foodplus.java
@@ -1,15 +1,30 @@
 package ru.theone_ss.foodplus;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import ru.theone_ss.foodplus.registry.FoodItems;
 
 public class Foodplus implements ModInitializer {
 
+	public static ItemGroup FOODPLUS;
+	public static final String MOD_ID = "foodplusid";
 
-	public void onInitialize() {
-
-
-		FoodItems.reg_items();
-
+	public void onInitialize()
+	{
+		registerItemGroup();
+		FoodItems.init();
+	}
+	public static void registerItemGroup()
+	{
+		FOODPLUS = FabricItemGroup.builder(new Identifier(Foodplus.MOD_ID, "foodplusid"))
+				.displayName(Text.translatable("Food+"))
+				.icon(() -> FoodItems.RED_SUS_CANDY.asItem().getDefaultStack()).build();
+	}
+	public static Identifier id(String path)
+	{
+		return new Identifier(MOD_ID, path);
 	}
 }
