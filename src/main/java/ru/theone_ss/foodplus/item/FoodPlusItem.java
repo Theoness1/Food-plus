aa9ad52f87c4ffb209418bb36d7299942952c0fe
Theoness1 <sasha.bolotskih2002@gmail.com>
2023-03-01 16:00:25 +0700

    clean

---
+++ src/main/java/ru/theone_ss/foodplus/item/FoodPlusItem.java
@@ -0,0 +1,9 @@
+package ru.theone_ss.foodplus.item;
+
+import net.minecraft.item.Item;
+
+public class FoodPlusItem extends Item {
+    public FoodPlusItem(Settings settings) {
+        super(settings);
+    }
+}
