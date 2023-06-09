1fda86afdb6565f2f64ea9ff0c7ec2bc9a4e2285
Theoness1 <sasha.bolotskih2002@gmail.com>
2023-06-09 13:12:12 +0700

    Bug fixed with shears

--- src/main/java/ru/theone_ss/foodplus/mixin/GlowLichenMixin.java
+++ src/main/java/ru/theone_ss/foodplus/mixin/GlowLichenMixin.java
@@ -3,10 +3,12 @@ package ru.theone_ss.foodplus.mixin;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -17,13 +19,12 @@ import java.util.random.RandomGenerator;
 
 @Mixin(Block.class)
 abstract class GlowLichenMixin {
-    @Inject(at = @At("HEAD"), method = "onBroken")
-    private void onBroken(WorldAccess world, BlockPos pos, BlockState state, CallbackInfo ci)
+    @Inject(at = @At("HEAD"), method = "onBreak")
+    private void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player, CallbackInfo ci)
     {
-        if(!world.isClient() && state.isOf(Blocks.GLOW_LICHEN))
-        {
-            Block.dropStack((World)world, pos, new ItemStack(FoodItems.LICHEN, RandomGenerator.getDefault().nextInt(1, 3)));
+        if(!world.isClient() && state.isOf(Blocks.GLOW_LICHEN) && !player.getStackInHand(Hand.MAIN_HAND).isOf(Items.SHEARS))
+        { //TODO
+            Block.dropStack(world, pos, new ItemStack(FoodItems.LICHEN, RandomGenerator.getDefault().nextInt(1, 3)));
         }
     }
-
 }
