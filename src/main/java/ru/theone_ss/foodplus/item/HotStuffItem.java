5e3a771a97e8e4c60557f8eda77c2b6e1ddc257d
Theoness1 <sasha.bolotskih2002@gmail.com>
2023-06-15 22:03:12 +0700

    add new class for hotstuff item

---
+++ src/main/java/ru/theone_ss/foodplus/item/HotStuffItem.java
@@ -0,0 +1,46 @@
+package ru.theone_ss.foodplus.item;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class HotStuffItem extends Item {
+
+    private final BlockState fireBlock = Blocks.FIRE.getDefaultState();
+
+    public HotStuffItem(Settings settings) {
+        super(settings);
+    }
+
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user)
+    {
+        super.finishUsing(stack, world, user);
+
+        BlockPos pos = user.getBlockPos();
+        world.setBlockState(pos, fireBlock);
+        user.setPosition(user.getX(), user.getY() + 1, user.getZ());
+
+        if (stack.isEmpty())
+        {
+            return new ItemStack(Items.BOWL);
+        }
+        else
+        {
+            if (user instanceof PlayerEntity playerEntity && !((PlayerEntity)user).getAbilities().creativeMode)
+            {
+                ItemStack itemStack = new ItemStack(Items.BOWL);
+                if (!playerEntity.getInventory().insertStack(itemStack))
+                {
+                    playerEntity.dropItem(itemStack, false);
+                }
+            }
+            return stack;
+        }
+    }
+}
